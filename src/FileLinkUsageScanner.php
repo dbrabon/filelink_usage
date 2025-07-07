a588916a59bcd962e476a3a3b5fe938bae4043d0
Daniel Boone Brabon <dbrabon@sandiego.gov>
2025-07-07 11:59:59 -0700

    Add scan status tracking and selective scanning

--- src/FileLinkUsageScanner.php
+++ src/FileLinkUsageScanner.php
@@ -24,17 +24,47 @@ class FileLinkUsageScanner {
     $this->configFactory = $configFactory;
   }
 
-  public function scan(): array {
+  public function scan(?array $nids = NULL): array {
     $results = [];
     $verbose = (bool) $this->configFactory->get('filelink_usage.settings')->get('verbose_logging');
 
     $storage = $this->entityTypeManager->getStorage('node');
-    $nids = $storage->getQuery()
-      ->accessCheck(TRUE)
-      ->execute();
+    if ($nids === NULL) {
+      $nids = $storage->getQuery()
+        ->accessCheck(TRUE)
+        ->execute();
+    }
 
     $nodes = $storage->loadMultiple($nids);
     foreach ($nodes as $node) {
+      // Remove old usage records and matches so we can store a fresh set.
+      $links = $this->database->select('filelink_usage_matches', 'f')
+        ->fields('f', ['link'])
+        ->condition('nid', $node->id())
+        ->execute()
+        ->fetchCol();
+
+      foreach ($links as $link) {
+        $uri = $link;
+        if (preg_match('#https?://[^/]+(/sites/default/files/.*)#i', $uri, $m)) {
+          $uri = $m[1];
+        }
+        if (strpos($uri, '/sites/default/files/') === 0) {
+          $uri = 'public://' . substr($uri, strlen('/sites/default/files/'));
+        }
+
+        $file_storage = $this->entityTypeManager->getStorage('file');
+        $files = $file_storage->loadByProperties(['uri' => $uri]);
+        $file = $files ? reset($files) : NULL;
+        if ($file) {
+          $this->fileUsage->delete($file, 'filelink_usage', 'node', $node->id());
+        }
+      }
+
+      $this->database->delete('filelink_usage_matches')
+        ->condition('nid', $node->id())
+        ->execute();
+
       foreach ($node->getFields() as $field) {
         if ($field->getFieldDefinition()->getType() === 'text_long' || $field->getFieldDefinition()->getType() === 'text_with_summary') {
           $text = $field->value;
@@ -49,8 +79,8 @@ class FileLinkUsageScanner {
             }
 
             $file_storage = $this->entityTypeManager->getStorage('file');
-            $file = $file_storage->loadByProperties(['uri' => $uri]);
-            $file = $file ? reset($file) : NULL;
+            $files = $file_storage->loadByProperties(['uri' => $uri]);
+            $file = $files ? reset($files) : NULL;
 
             if ($file) {
               $usage = $this->fileUsage->listUsage($file);
@@ -83,6 +113,12 @@ class FileLinkUsageScanner {
           }
         }
       }
+
+      // Record successful scan time.
+      $this->database->merge('filelink_usage_scan_status')
+        ->key(['nid' => $node->id()])
+        ->fields(['scanned' => time()])
+        ->execute();
     }
 
     $this->logger->info('Scanned @count nodes for file links.', ['@count' => count($nodes)]);
