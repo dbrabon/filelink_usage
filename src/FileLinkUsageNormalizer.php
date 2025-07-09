0a90c933b7a44690ea20ccd32411695ec0b2510f
Daniel Boone Brabon <dbrabon@sandiego.gov>
2025-07-09 14:01:23 -0700

    bug fixes

--- src/FileLinkUsageNormalizer.php
+++ src/FileLinkUsageNormalizer.php
@@ -5,31 +5,43 @@ namespace Drupal\filelink_usage;
 
 use Drupal\Component\Utility\Html;
 
+/**
+ * Converts any form of link to a canonical `public://…` URI.
+ */
 class FileLinkUsageNormalizer {
 
+  /**
+   * Normalize a link so scanners & lookups are consistent.
+   *
+   * @param string $link
+   *   Raw link captured from HTML.
+   *
+   * @return string
+   *   Canonical URI (e.g. `public://abc.pdf`).
+   */
   public function normalize(string $link): string {
-    // Decode HTML entities and trim common surrounding characters.
+    // Decode entities and strip quotes / whitespace.
     $link = Html::decodeEntities($link);
     $link = trim($link, " \t\n\r\0\x0B\"'");
 
-    // If already a public:// URI, return as-is.
+    // If it is already public://, leave it untouched.
     if (strpos($link, 'public://') === 0) {
       return $link;
     }
 
-    // Strip hostnames from absolute URLs.
+    // Strip host portion from absolute URLs.
     if (preg_match('#^https?://[^/]+(/.*)$#i', $link, $m)) {
       $link = $m[1];
     }
 
-    // Collapse repeated slashes.
+    // Collapse duplicate slashes.
     $link = preg_replace('#/{2,}#', '/', $link);
 
-    // Remove query strings and fragments.
+    // Remove query strings / fragments.
     $link = preg_replace('/[\?#].*$/', '', $link);
     $link = rtrim($link, '/');
 
-    // Convert sites/default/files paths to public scheme.
+    // Turn /sites/default/files/... into public://...
     if (strpos($link, '/sites/default/files/') === 0) {
       $link = 'public://' . substr($link, strlen('/sites/default/files/'));
     }
